b943b7169ca7d69960978d4d5ae3bf5ef9c4c8b3
mdoblr <mdoblr@outlook.com>
2015-02-25 17:35:56 -0800

    optimized code

--- u2l3_challenge.py
+++ u2l3_challenge.py
@@ -7,8 +7,11 @@ Created on Fri Feb 20 18:20:29 2015
 import sqlite3 as lite
 import pandas as pd
 con = lite.connect('getting_started.db')
+cur = con.cursor()
 
-cities = (('New York City', 'NY'),
+#this was just a tuple of tuples, but modifying with a SQLite statetment
+cities_populate = """INSERT into cities (name, state) VALUES 
+    ('New York City', 'NY'),
     ('Boston', 'MA'),
     ('Chicago', 'IL'),
     ('Miami', 'FL'),
@@ -16,30 +19,39 @@ cities = (('New York City', 'NY'),
     ('Seattle', 'WA'),
     ('Portland', 'OR'),
     ('San Francisco', 'CA'),
-    ('Los Angeles', 'CA'))
-    
-weather = (('New York City', 2013, 'July', 'January', 62),
-           ('Boston', 2013, 'July', 'January', 59), 
+    ('Los Angeles', 'CA');"""
+
+#Doing the same as above   
+weather_populate = """INSERT into weather (city, year, warm_month, cold_month, average_h) VALUES
+            ('New York City', 2013, 'July', 'January', 62),
+            ('Boston', 2013, 'July', 'January', 59), 
             ('Chicago', 2013, 'July', 'January', 59), 
             ('Miami', 2013, 'August', 'January', 84), 
             ('Dallas', 2013, 'July','January', 77), 
             ('Seattle', 2013, 'July', 'January', 61), 
             ('Portland', 2013, 'July', 'December', 63), 
             ('San Francisco', 2013, 'September', 'December', 64), 
-            ('Los Angeles', 2013, 'September', 'December', 75))
+            ('Los Angeles', 2013, 'September', 'December', 75);"""
 
 
-with con:
-    cur = con.cursor()    
-    cur.execute('drop table if exists cities;')
-    cur.execute('drop table if exists weather;')
-    cur.execute('create table cities (name text, state text);')
-    cur.execute('create table weather (city text, year integer, warm_month text, cold_month text, average_h integer);')
-    cur.executemany('insert into weather values (?,?,?,?,?)', weather)
-    cur.executemany('insert into cities values (?,?)', cities)
-    cur.execute('SELECT name, state, average_h FROM cities LEFT OUTER JOIN weather ON name = city;')
-    rows = cur.fetchall()
-    df = pd.DataFrame(rows)
+#creating tables in database with new values   
+cur.execute('drop table if exists cities;')
+cur.execute('drop table if exists weather;')
+cur.execute('create table cities (name text, state text);')
+cur.execute('create table weather (city text, year integer, warm_month text, cold_month text, average_h integer);')
 
-    
-    
+#insert values using python variable containing sqlite command
+cur.execute(cities_populate)
+cur.execute(weather_populate)
+
+#joining tables together
+cities_weather = cur.execute('SELECT name, state, average_h FROM cities LEFT OUTER JOIN weather ON name = city;')
+
+#feeding data into panda dataframe
+df_tables = pd.read_sql(cities_weather, con)
+
+#indexing to select the months that are warmest in july
+julying = df_tables(df_tables(['average_h' == 'July']))
+
+#printing
+print "cities that are warmest in July are:", ', '.join(julying.tolist())
