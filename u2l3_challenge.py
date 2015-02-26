b7f5a33219fe577b77cef1e3e2dde6c136a5e0f4
mdoblr <mdoblr@outlook.com>
2015-02-25 17:37:34 -0800

    optimized code

--- u2l3_challenge.py
+++ u2l3_challenge.py
@@ -1,16 +1,13 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Feb 20 18:20:29 2015
-
-@author: mdblr
-"""
 import sqlite3 as lite
 import pandas as pd
+
+
+""" Connect to the database """
 con = lite.connect('getting_started.db')
 cur = con.cursor()
 
-#this was just a tuple of tuples, but modifying with a SQLite statetment
-cities_populate = """INSERT into cities (name, state) VALUES 
+populate_cities = """
+INSERT INTO cities (name, state) VALUES
     ('New York City', 'NY'),
     ('Boston', 'MA'),
     ('Chicago', 'IL'),
@@ -19,39 +16,48 @@ cities_populate = """INSERT into cities (name, state) VALUES
     ('Seattle', 'WA'),
     ('Portland', 'OR'),
     ('San Francisco', 'CA'),
-    ('Los Angeles', 'CA');"""
+    ('Los Angeles', 'CA');
+"""
+populate_weather = """
+INSERT INTO weather (city,year,warm_month,cold_month,average_high) VALUES
+    ('New York City',2013,'July','January',62),
+    ('Boston',2013,'July','January',59),
+    ('Chicago',2013,'July','January',59),
+    ('Miami',2013,'August','January',84),
+    ('Dallas',2013,'July','January',77),
+    ('Seattle',2013,'July','January',61),
+    ('Portland',2013,'July','December',63),
+    ('San Francisco',2013,'September','December',64),
+    ('Los Angeles',2013,'September','December',75);
+"""
 
-#Doing the same as above   
-weather_populate = """INSERT into weather (city, year, warm_month, cold_month, average_h) VALUES
-            ('New York City', 2013, 'July', 'January', 62),
-            ('Boston', 2013, 'July', 'January', 59), 
-            ('Chicago', 2013, 'July', 'January', 59), 
-            ('Miami', 2013, 'August', 'January', 84), 
-            ('Dallas', 2013, 'July','January', 77), 
-            ('Seattle', 2013, 'July', 'January', 61), 
-            ('Portland', 2013, 'July', 'December', 63), 
-            ('San Francisco', 2013, 'September', 'December', 64), 
-            ('Los Angeles', 2013, 'September', 'December', 75);"""
+
+""" Create the cities and weather tables """
+cur.execute("DROP TABLE IF EXISTS cities")
+cur.execute("CREATE TABLE cities (name text, state text)")
+cur.execute("DROP TABLE IF EXISTS weather")
+cur.execute("CREATE TABLE weather (city text,year integer,warm_month text,cold_month text,average_high integer)")
+
+""" Populate the tables """
+cur.execute(populate_weather)
+cur.execute(populate_cities)
+
+""" Load into pandas data-frame"""
+query_cities = "select * from cities"
+cities = pd.read_sql(query_cities,con)
 
 
-#creating tables in database with new values   
-cur.execute('drop table if exists cities;')
-cur.execute('drop table if exists weather;')
-cur.execute('create table cities (name text, state text);')
-cur.execute('create table weather (city text, year integer, warm_month text, cold_month text, average_h integer);')
+query_weather = "select * from weather"
+weather = pd.read_sql(query_weather,con)
+weather["name"] = weather["city"]
+weather.drop('city', axis=1, inplace=True)
 
-#insert values using python variable containing sqlite command
-cur.execute(cities_populate)
-cur.execute(weather_populate)
+""" join two data frames """
+combined = pd.DataFrame.merge(cities,weather, how='inner', left_on = 'name', right_on = 'name')
 
-#joining tables together
-cities_weather = cur.execute('SELECT name, state, average_h FROM cities LEFT OUTER JOIN weather ON name = city;')
+""" pick the records where July is the warmest month """
+combined_july = combined[combined['warm_month'] == 'July']
 
-#feeding data into panda dataframe
-df_tables = pd.read_sql(cities_weather, con)
-
-#indexing to select the months that are warmest in july
-julying = df_tables(df_tables(['average_h' == 'July']))
-
-#printing
-print "cities that are warmest in July are:", ', '.join(julying.tolist())
+""" Print out the resulting city and state in a full sentence. For example The cities that are warmest in July are: Las Vegas, NV, Atlanta, GA """
+together = combined_july.apply(lambda x:'%s, %s' % (x['name'],x['state']),axis=1)
+print "cities that are warmest in July are:", ', '.join(together.tolist())
